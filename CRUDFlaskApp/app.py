f5d5e8043a468487642f97e82428b3ed04e63b4e
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-08-09 08:57:49 +0530

    Update app.py

--- CRUDFlaskApp/app.py
+++ CRUDFlaskApp/app.py
@@ -7,8 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///customer.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = "secretkey"
+app.config['SECRET_KEY'] = "secretkey" #random key to encrypt cookies
 
 db = SQLAlchemy(app)
 
